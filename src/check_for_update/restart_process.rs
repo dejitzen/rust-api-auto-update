ed84df5cdcf7d957346f02460f6a3d37bbdd924d
Deji Abodunrin <dejitzen@Dejis-MacBook-Pro.local>
2024-02-13 18:33:41 +0000

    quick cargo fix

--- src/check_for_update/restart_process.rs
+++ src/check_for_update/restart_process.rs
@@ -2,5 +2,5 @@ use std::{os::unix::process::CommandExt, process::Command};
 
 pub fn restart_process() {
     println!("Starting restart");
-    let output = Command::new("./target/release/rust-api-auto-update").exec();
+    let _output = Command::new("./target/release/rust-api-auto-update").exec();
 }
